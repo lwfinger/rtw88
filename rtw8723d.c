9c99d5d2f63b345e822680559226a5a0b5d9341c
Larry Finger <Larry.Finger@lwfinger.net>
2023-07-31 12:42:03 -0500

    rtw88: Add dump of MAC ID
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- rtw8723d.c
+++ rtw8723d.c
@@ -220,6 +220,7 @@ static void rtw8723ds_efuse_parsing(struct rtw_efuse *efuse,
 				    struct rtw8723d_efuse *map)
 {
 	ether_addr_copy(efuse->addr, map->s.mac_addr);
+	pr_info("****** rtw88: efuse->addr %pM\n", efuse->addr);
 }
 
 static int rtw8723d_read_efuse(struct rtw_dev *rtwdev, u8 *log_map)
