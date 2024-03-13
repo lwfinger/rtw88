0c9169b653d12041000caec3e7055f7d20ffc311
Larry Finger <Larry.Finger@gmail.com>
2024-03-13 14:38:03 -0500

    rtw88: Add additional vid/pid pairs for 8821au
    This file was prepared by Nick Morrow.
    
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- rtw8821au.c
+++ rtw8821au.c
@@ -9,60 +9,58 @@
 #include "usb.h"
 
 static const struct usb_device_id rtw_8821au_id_table[] = {
-        /*=== Realtek demoboard ===*/
 	{ USB_DEVICE_AND_INTERFACE_INFO(RTW_USB_VENDOR_ID_REALTEK, 0x0811, 0xff, 0xff, 0xff),
-	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* 8821AU */
+	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) },
 	{ USB_DEVICE_AND_INTERFACE_INFO(RTW_USB_VENDOR_ID_REALTEK, 0x0820, 0xff, 0xff, 0xff),
-	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* 8821AU */
+	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) },
 	{ USB_DEVICE_AND_INTERFACE_INFO(RTW_USB_VENDOR_ID_REALTEK, 0x0821, 0xff, 0xff, 0xff),
-	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* 8821AU */
-	{ USB_DEVICE_AND_INTERFACE_INFO(RTW_USB_VENDOR_ID_REALTEK, 0x0822, 0xff, 0xff, 0xff),
-	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* 8821AU */
+	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) },
+	{ USB_DEVICE_AND_INTERFACE_INFO(RTW_USB_VENDOR_ID_REALTEK, 0x8822, 0xff, 0xff, 0xff),
+	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) },
 	{ USB_DEVICE_AND_INTERFACE_INFO(RTW_USB_VENDOR_ID_REALTEK, 0x0823, 0xff, 0xff, 0xff),
-	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* 8821AU */
+	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) },
 	{ USB_DEVICE_AND_INTERFACE_INFO(RTW_USB_VENDOR_ID_REALTEK, 0xa811, 0xff, 0xff, 0xff),
-	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* 8821AU */
-        /*=== Customer ID ===*/
+	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) },
 	{ USB_DEVICE_AND_INTERFACE_INFO(0x0411, 0x0242, 0xff, 0xff, 0xff),
-	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* Elecom - WDC-433DU2H */
-	{ USB_DEVICE_AND_INTERFACE_INFO(0x0411, 0x0298, 0xff, 0xff, 0xff),
-	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* Buffalo - WI-U2-433DHP */
-	{ USB_DEVICE_AND_INTERFACE_INFO(0x0488, 0x0953, 0xff, 0xff, 0xff),
-	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* I-O DATA - Edimax */
+	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* Buffalo */
+	{ USB_DEVICE_AND_INTERFACE_INFO(0x0411, 0x029B, 0xff, 0xff, 0xff),
+	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* Buffalo */
+	{ USB_DEVICE_AND_INTERFACE_INFO(0x04bb, 0x0953, 0xff, 0xff, 0xff),
+	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* I-O DATA */
 	{ USB_DEVICE_AND_INTERFACE_INFO(0x056e, 0x4007, 0xff, 0xff, 0xff),
-	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* Elecom WDC-433DU2HBK */
+	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* ELECOM */
 	{ USB_DEVICE_AND_INTERFACE_INFO(0x056e, 0x400e, 0xff, 0xff, 0xff),
-	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* Elecom */
+	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* ELECOM */
 	{ USB_DEVICE_AND_INTERFACE_INFO(0x056e, 0x400f, 0xff, 0xff, 0xff),
-	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* Elecom */
+	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* ELECOM */
 	{ USB_DEVICE_AND_INTERFACE_INFO(0x0846, 0x9052, 0xff, 0xff, 0xff),
-	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* Netgear - A6100 */
+	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* Netgear */
 	{ USB_DEVICE_AND_INTERFACE_INFO(0x0e66, 0x0023, 0xff, 0xff, 0xff),
-	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* Hawking - Edimax */
+	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* HAWKING */
 	{ USB_DEVICE_AND_INTERFACE_INFO(0x2001, 0x3314, 0xff, 0xff, 0xff),
-	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* D-Link - Cameo */
+	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* D-Link */
 	{ USB_DEVICE_AND_INTERFACE_INFO(0x2001, 0x3318, 0xff, 0xff, 0xff),
-	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* D-Link - Cameo */
+	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* D-Link */
 	{ USB_DEVICE_AND_INTERFACE_INFO(0x2019, 0xab32, 0xff, 0xff, 0xff),
-	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* Planex - GW-450S */
+	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* Planex */
 	{ USB_DEVICE_AND_INTERFACE_INFO(0x20f4, 0x804b, 0xff, 0xff, 0xff),
-	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* TrendNet */
+	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* TRENDnet */
 	{ USB_DEVICE_AND_INTERFACE_INFO(0x2357, 0x011e, 0xff, 0xff, 0xff),
-	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* TP-Link T2U Nano */
+	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* TP Link */
 	{ USB_DEVICE_AND_INTERFACE_INFO(0x2357, 0x011f, 0xff, 0xff, 0xff),
-	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* TP-Link Archer AC600 T2U Nano */
+	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* TP Link */
 	{ USB_DEVICE_AND_INTERFACE_INFO(0x2357, 0x0120, 0xff, 0xff, 0xff),
-	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* TP-Link T2U Plus and Archer T600U Plus */
+	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* TP Link */
 	{ USB_DEVICE_AND_INTERFACE_INFO(0x3823, 0x6249, 0xff, 0xff, 0xff),
-	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* Obihai - OBiWiFi */
+	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* Obihai */
 	{ USB_DEVICE_AND_INTERFACE_INFO(0x7392, 0xa811, 0xff, 0xff, 0xff),
 	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* Edimax */
 	{ USB_DEVICE_AND_INTERFACE_INFO(0x7392, 0xa812, 0xff, 0xff, 0xff),
-	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* Edimax EW-7811UTC */
+	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* Edimax */
 	{ USB_DEVICE_AND_INTERFACE_INFO(0x7392, 0xa813, 0xff, 0xff, 0xff),
-	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* Edimax EW-7811UAC */
+	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* Edimax */
 	{ USB_DEVICE_AND_INTERFACE_INFO(0x7392, 0xb611, 0xff, 0xff, 0xff),
-	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* Edimax EW-7811UCB */
+	  .driver_info = (kernel_ulong_t)&(rtw8821a_hw_spec) }, /* Edimax */
 	{},
 };
 MODULE_DEVICE_TABLE(usb, rtw_8821au_id_table);
@@ -81,7 +79,6 @@ static struct usb_driver rtw_8821au_driver = {
 };
 module_usb_driver(rtw_8821au_driver);
 
-MODULE_AUTHOR("Larry Finger <Larry.Finger@lwfinger.net>");
-
-MODULE_DESCRIPTION("Realtek 802.11ac wireless 8821au driver");
+MODULE_AUTHOR("Hans Ulli Kroll <linux@ulli-kroll.de>");
+MODULE_DESCRIPTION("Realtek 802.11ac wireless 8821cu driver");
 MODULE_LICENSE("Dual BSD/GPL");
