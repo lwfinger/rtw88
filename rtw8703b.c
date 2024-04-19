f2f3c87f07678a7ff37cd0a70feacbd68e757258
Larry Finger <Larry.Finger@gmail.com>
2024-04-19 12:32:56 -0500

    rtw88: Fix build of rtw8703b.c under kernel 5.4
    An include was missing.
    
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- rtw8703b.c
+++ rtw8703b.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause
 /* Copyright Fiona Klute <fiona.klute@gmx.de> */
 
+#include <linux/of.h>
 #include <linux/of_net.h>
 #include "main.h"
 #include "coex.h"
