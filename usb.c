694fbaa83ed51e536bbcbd7ddcffe7bfcc1169b4
Marco Crivellari <marco.crivellari@suse.com>
2026-02-26 22:47:01 +0800

    wifi: rtw88: add WQ_PERCPU to alloc_workqueue users
    Currently if a user enqueues a work item using schedule_delayed_work() the
    used wq is "system_wq" (per-cpu wq) while queue_delayed_work() use
    WORK_CPU_UNBOUND (used when a cpu is not specified). The same applies to
    schedule_work() that is using system_wq and queue_work(), that makes use
    again of WORK_CPU_UNBOUND.
    
    This lack of consistency cannot be addressed without refactoring the API.
    For more details see the Link tag below.
    
    alloc_workqueue() treats all queues as per-CPU by default, while unbound
    workqueues must opt-in via WQ_UNBOUND.
    
    This default is suboptimal: most workloads benefit from unbound queues,
    allowing the scheduler to place worker threads where they’re needed and
    reducing noise when CPUs are isolated.
    
    This continues the effort to refactor workqueue APIs, which began with
    the introduction of new workqueues and a new alloc_workqueue flag in:
    
    commit 128ea9f6ccfb ("workqueue: Add system_percpu_wq and system_dfl_wq")
    commit 930c2ea566af ("workqueue: Add new WQ_PERCPU flag")
    
    This change adds a new WQ_PERCPU flag to explicitly request
    alloc_workqueue() to be per-cpu when WQ_UNBOUND has not been specified.
    
    With the introduction of the WQ_PERCPU flag (equivalent to !WQ_UNBOUND),
    any alloc_workqueue() caller that doesn’t explicitly specify WQ_UNBOUND
    must now use WQ_PERCPU.
    
    Once migration is complete, WQ_UNBOUND can be removed and unbound will
    become the implicit default.
    
    Suggested-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Marco Crivellari <marco.crivellari@suse.com>
    Link: https://lore.kernel.org/all/20250221112003.1dSuoGyc@linutronix.de/
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>
    Link: https://patch.msgid.link/20251113160605.381777-3-marco.crivellari@suse.com

--- usb.c
+++ usb.c
@@ -995,7 +995,10 @@ static int rtw_usb_init_rx(struct rtw_dev *rtwdev)
 	struct sk_buff *rx_skb;
 	int i;
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 9, 0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 18, 0)
+	rtwusb->rxwq = alloc_workqueue("rtw88_usb: rx wq", WQ_BH | WQ_PERCPU,
+				       0);
+#elif LINUX_VERSION_CODE >= KERNEL_VERSION(6, 9, 0)
 	rtwusb->rxwq = alloc_workqueue("rtw88_usb: rx wq", WQ_BH, 0);
 #else
 	tasklet_init(&rtwusb->rx_tasklet, rtw_usb_rx_handler,
